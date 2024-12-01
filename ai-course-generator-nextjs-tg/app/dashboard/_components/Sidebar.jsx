5b2141f8b37e45a4f30241dbfbc55ecf4cab9987
Muhammad Musa Mueed <musavuswh@gmail.com>
2024-12-01 18:11:37 +0500

    added progressbar, part-1

--- ai-course-generator-nextjs-tg/app/dashboard/_components/Sidebar.jsx
+++ ai-course-generator-nextjs-tg/app/dashboard/_components/Sidebar.jsx
@@ -4,13 +4,14 @@ import Image from "next/image";
 import Link from "next/link";
 import { menuLists } from "@/Constants/contants";
 import { usePathname } from "next/navigation";
+import { Progress } from "@/components/ui/progress";
 
 export default function Sidebar() {
 
   const pathname = usePathname();
   
   return (
-    <div className="h-full w-full bg-white shadow-lg">
+    <div className="h-full w-full bg-white shadow-lg relative">
       {/* Inner Container - Sidebar Section */}
       <div>
         {/* Logo - Section */}
@@ -39,7 +40,15 @@ export default function Sidebar() {
         <ul className="px-2 space-y-1 my-6">
           {menuLists.map((item, index) => (
             // pathname.includes(item.href)
-            <Link key={index} href={item.href} className={`flex flex-col p-2 rounded-md ${pathname === item.href ? "bg-blue-100/80 text-black" : "hover:bg-gray-100"}`}>
+            <Link
+              key={index}
+              href={item.href}
+              className={`flex flex-col p-2 rounded-md ${
+                pathname === item.href
+                  ? "bg-blue-100/80 text-black"
+                  : "hover:bg-gray-100"
+              }`}
+            >
               <div className="flex items-center space-x-2">
                 {item.icon}
                 <span className="text-lg font-medium">{item.label}</span>
@@ -47,6 +56,17 @@ export default function Sidebar() {
             </Link>
           ))}
         </ul>
+
+        <div className="absolute bottom-10 w-full">
+          {/* Progress Bar - Section */}
+          <div className="w-[90%] sm:w-[85%] mx-auto">
+            <Progress value={60} />
+            <h2 className="text-sm my-2">3 Out of 5 Course created</h2>
+            <h2 className="text-xs text-gray-500">
+              Upgrade your plan for unlimited course generation
+            </h2>
+          </div>
+        </div>
       </div>
     </div>
   );
