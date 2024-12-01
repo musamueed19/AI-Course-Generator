a38d901600df124fac23c98cbc2dc34dfa57144a
Muhammad Musa Mueed <musavuswh@gmail.com>
2024-12-01 18:39:45 +0500

    added Responsiveness for Mobile in Greets and CreateCourse Button

---
+++ ai-course-generator-nextjs-tg/app/create-course/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function CreateCoursePage() {
+  return (
+      <div>
+          
+          {/* Stepper */}
+          
+          {/* Component - based on State */}
+
+
+          {/* Next Previous - Button Section */}
+
+
+    </div>
+  )
+}
--- ai-course-generator-nextjs-tg/app/dashboard/_components/AddCourse.jsx
+++ ai-course-generator-nextjs-tg/app/dashboard/_components/AddCourse.jsx
@@ -4,6 +4,7 @@ import { IoMdAdd } from "react-icons/io";
 
 import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
 
 
 export default function AddCourse() {
@@ -11,19 +12,24 @@ export default function AddCourse() {
   const { user } = useUser();
 
   return (
-    <div className="flex items-center justify-between">
-
+    <div className="md:flex items-center justify-between inline-block">
       {/* Greetings, and Help Tip - Section */}
-      <div>
+      <div className="mb-4 md:mb-0">
         <h2 className="text-lg font-medium">
-          Hello, Assalam-o-Alaikum! <span className="text-xl font-bold">{user?.fullName}</span>
+          Hello, Assalam-o-Alaikum!{" "}
+          <span className="text-xl font-bold">{user?.fullName}</span>
         </h2>
 
         <p>Create new course with AI, Share with friends and Earn from it</p>
       </div>
 
       {/* Call to Action - Create Course - Button Section */}
-      <Button><IoMdAdd className="w-10 h-10" />Create AI Course</Button>
+      <Link href="/create-course">
+        <Button>
+          <IoMdAdd className="w-10 h-10" />
+          Create AI Course
+        </Button>
+      </Link>
     </div>
   );
 }
